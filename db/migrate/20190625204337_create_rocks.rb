4f4194d5ee5e7d7513ce51a6289313c59def82bd
Tony Baker <this.tonybaker@gmail.com>
2019-06-25 13:53:55 -0700

    filled out attribute/columns for rocks table

--- db/migrate/20190625204337_create_rocks.rb
+++ db/migrate/20190625204337_create_rocks.rb
@@ -1,4 +1,10 @@
 class CreateRocks < ActiveRecord::Migration[5.2]
   def change
+    create_table :rocks do |t|
+      t.integer :user_id
+      t.string :name
+      t.string :description
+      t.string :location
+    end
   end
 end
